19214cb9831fbad535e8cf480828cf11e76612a0
leonardoedgar <leonardo_edgar98@outlook.com>
2020-04-13 01:36:53 +0800

    implement room service ordering and guest check out

--- src/resources/Payment.java
+++ src/resources/Payment.java
@@ -1,21 +1,72 @@
 package resources;
+import java.util.ArrayList;
 
+/**
+ * A class that represents payment in a hotel.
+ */
 public class Payment {
-//	double totalCost;
-	String PaymentType;
-	double roomCost;
-	double roomServiceCost;
+	private String paymentType;
+	private String roomType;
+	private double roomCost;
+	private ArrayList<RoomService> roomServiceList;
+	private double taxRate;
+	private double discountRate;
 	
-	public double getTotalCost(Guest guest){
-		return guest.total_cost;
+	/**
+	 * A class constructor to create payment object
+	 * @param paymentType {String} indicates the payment type
+	 * @param roomType {String} the room type the person stayed at the hotel
+	 * @param roomCost {Double} the room cost the person stayed at the hotel
+	 * @param roomServiceList {ArrayList<RoomService>} the room services that are made by the guest
+	 * @param taxRate {Double} tax rate for the payment
+	 * @param discountRate {Double} discount for the payment
+	 */
+	public Payment(String paymentType, String roomType, double roomCost, 
+			ArrayList<RoomService> roomServiceList, double taxRate, double discountRate) {
+		this.paymentType = paymentType;
+		this.roomType = roomType;
+		this.roomCost = roomCost;
+		this.roomServiceList = roomServiceList;
+		this.taxRate = taxRate;
+		this.discountRate = discountRate;
 	}
 	
-	private void sumTotalCost() {
-		
+	public Payment(String paymentType, String roomType, double roomCost, 
+			ArrayList<RoomService> roomServiceList) {
+		this(paymentType, roomType, roomCost, roomServiceList, 0.07, 0);
 	}
 	
-	public double payment(Guest guest) {
-		
+	public Payment(String paymentType, String roomType, double roomCost, 
+			ArrayList<RoomService> roomServiceList, double taxRate) {
+		this(paymentType, roomType, roomCost, roomServiceList, taxRate, 0);
+	}
+	
+	/**
+	 * A function to print the payment receipt.
+	 */
+	public void printReceipt() {
+		double totalCost = this.roomCost;
+		System.out.println("           HOTEL CHECKOUT");
+		System.out.println("===================================");
+		System.out.println("Room type: " + this.roomType);
+		System.out.println("Room cost: SGD" + this.roomCost);
+		System.out.println("Room order history: ");
+		for (RoomService roomService: roomServiceList) {
+			roomService.printOrderHistory();
+			totalCost += roomService.getTotalCost();
+		}
+		System.out.println("===================================");
+		System.out.println("Total cost before tax                        : SGD" + totalCost);
+		double discountAmount = this.discountRate*totalCost;
+		totalCost -= discountAmount;
+		System.out.println("Discount rate(" + this.discountRate*100 + "%): SGD" + discountAmount);
+		double taxAmount = this.taxRate*totalCost;
+		totalCost += taxAmount;
+		System.out.println("Tax rate("      + this.taxRate*100      + "%): SGD" + taxAmount);
+		System.out.println("Total cost after tax                         : SGD" + totalCost);
+		System.out.println("===================================");
+		System.out.println("  THANK YOU FOR STAYING WITH US!");
+		System.out.println("           SEE YOU AGAIN!");
 	}
 }
 
--- src/resources/RoomService.java
+++ src/resources/RoomService.java
@@ -1,7 +1,82 @@
 package resources;
+import java.util.Hashtable;
+import java.util.Date;
+import exception.FoodNotOnMenuException;
 import resources.Menu;
 
+/**
+ * A class to represent a room service in a hotel.
+ */
 public class RoomService {
-	Menu menu;
+	private Menu menu;
+	private double totalCost;
+	private Hashtable<String, String> orderMap;
+	private Date dateOfTransaction;
+	/**
+	 * A class constructor to construct a room service.
+	 * @param menu {Menu} the room service menu in a hotel
+	 */
+	public RoomService(Menu menu) {
+		this.menu = menu;
+	}
 	
+	/**
+	 * A function to make an order.
+	 * @param orderMap {Hashtable<String, String>} the order and quantity map
+	 * @throws {FoodNotOnMenuException} when food ordered is not on the menu
+	 */
+	public void makeOrder(Hashtable<String, String> orderMap) 
+			throws FoodNotOnMenuException {
+		this.updateTotalCost(orderMap);
+		this.orderMap = orderMap;
+		this.dateOfTransaction = new Date();
+	}
+	
+	/**
+	 * A function to update the total cost of the room service.
+	 * @param orderMap {Hashtable<String, String>} the order and quantity map
+	 * @throws {FoodNotOnMenuException} when food ordered is not on the menu
+	 */
+	private void updateTotalCost(Hashtable<String, String> orderMap) 
+			throws FoodNotOnMenuException {
+		double cost = 0;
+		for (String foodName: orderMap.keySet()) {
+			cost += this.menu.getCostOfAFood(foodName)*Integer.parseInt(
+					orderMap.get(foodName));
+		}
+		this.totalCost = cost;
+	}
+	
+	/**
+	 * A function to print room service order.
+	 */
+	public void printOrderHistory() {
+		if (this.totalCost != 0) {
+			System.out.println("-----------------------------------");
+			System.out.println("Date of transaction: " + this.dateOfTransaction.toString());
+			for (String foodName: this.orderMap.keySet()) {
+				String foodQuantity = this.orderMap.get(foodName);
+				try {
+					double costPerFood = 
+							this.menu.getCostOfAFood(foodName)*Integer.parseInt(foodQuantity);
+					System.out.println(foodQuantity + " " + foodName + ": SGD" + 
+						Double.toString(costPerFood));	
+					System.out.println("-----------------------------------");
+				} catch (FoodNotOnMenuException e) {
+					continue;
+				}
+			}
+		}
+		else {
+			System.out.println("There is no order yet!.");
+		}
+	}
+	
+	/**
+	 * A function to get the total cost of the room service order.
+	 * @return {double} the total cost
+	 */
+	public double getTotalCost() {
+		return this.totalCost;
+	}
 }
